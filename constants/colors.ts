5089d41545dee3334eceafc86cc49815170b99ee
Caio Bernardo <caiobernardo.dev@outlook.com>
2025-11-06 12:48:09 -0300

    Initial Setup
    - Base folders created;
    - Constant files created;
    - Main assets.

---
+++ constants/colors.ts
@@ -0,0 +1,15 @@
+const colors = {
+  green: '#4A7C59',
+  black: '#1E1B18',
+  yellow: '#D9BB62',
+  beige: '#F2EDE4',
+  orangeLight: '#D98C4A',
+  orangeDark: '#D9653B',
+  red: '#D93829',
+
+  gradient: {
+    warm: ['#D9BB62', '#D9653B'] as const,
+  },
+};
+
+export default colors;
---
+++ constants/screen.ts
@@ -0,0 +1,6 @@
+import { Dimensions } from "react-native";
+
+const { height } = Dimensions.get("window");
+const paddingTop = height * 0.10;
+
+export default paddingTop
