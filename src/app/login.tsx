dbb9600fdecfd5a700a31ef7b79bf4850c0b8acf
ProjetoSofis <projetosofis@outlook.com>
2025-11-22 23:26:24 -0300

    feat: login logic implemented

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -1,49 +1,82 @@
 import colors from '@/constants/colors';
 import paddingTop from '@/constants/screen';
 import { LinearGradient } from 'expo-linear-gradient';
-import { Link, router } from 'expo-router';
+import { router } from 'expo-router';
 import { useState } from 'react';
-import { KeyboardAvoidingView, Platform, Pressable, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { ActivityIndicator, Pressable, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useAuth } from '../context/AuthContext';
 
 export default function Login() {
+  const { login, login2FA, isLoading } = useAuth();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [loading, setLoading] = useState(false);
+  const [twoFactorCode, setTwoFactorCode] = useState('');
 
+  const [is2FAMode, setIs2FAMode] = useState(false);
+  const [apiError, setApiError] = useState('')
   const [errors, setErrors] = useState({
     email: false,
-    password: false
+    password: false,
+    code: false,
   });
-
-  function HandleSignin() {
+  // Vinicius: função de login inicial
+  async function HandleSignin() {
+    setApiError('');
     const newErrors = {
       email: !email.trim(),
       password: !password.trim(),
+      code: false
     };
 
     setErrors(newErrors);
+    if (newErrors.email || newErrors.password) {
+      return;
+    }
+    
+    const result = await login(email, password);
+    console.log("Result: ", result);
 
-    const hasError = Object.values(newErrors).some(Boolean)
-    if (hasError) return;
+    if (result.status === 'REQUIRES_2FA') {
+      setIs2FAMode(true);
+    } else if (result.status === 'SUCCESS') {
+      console.log(result.status)
+      router.replace('/(tabs)/home');
+    } else {
+      setApiError(result.message || 'Erro ao acessar a conta')
+    }
 
-    setLoading(true)
-    console.log({ email, password })
-    setLoading(false)
+    // const hasError = Object.values(newErrors).some(Boolean)
+    // if (hasError) return;
 
-    router.replace('/(tabs)/home');
+    // setLoading(true)
+    // console.log({ email, password })
+    // setLoading(false)
+
+    // router.replace('/(tabs)/home');
+  }
+
+  async function HandleConfirm2FA() {
+    setApiError('');
+    console.log(twoFactorCode);
+
+    if (!twoFactorCode || twoFactorCode.length < 6) {
+      setErrors(prev => ({...prev, code: true}));
+      return;
+    }
+    const result = await login2FA(email, twoFactorCode);
+
+    if (result.status === 'SUCCESS') {
+      router.replace('/(tabs)/home');
+    } else {
+      setApiError(result.message || 'Código inválido ou expirado');
+    }
   }
 
   return (
 
-    <KeyboardAvoidingView
-      style={{ flex: 1 }}
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-    >
-      <ScrollView
-        style={{ flex: 1, backgroundColor: colors.beige }}
-        contentContainerStyle={{ flexGrow: 1 }}
-        keyboardShouldPersistTaps="handled"
-      >
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView style={{ flex: 1, backgroundColor: colors.beige }}>
         <LinearGradient
           colors={colors.gradient.warm}
           start={{ x: 0, y: 0 }}
@@ -56,11 +89,77 @@ export default function Login() {
             </Text>
             <Text style={styles.slogan}>
               Facilitando o cuidado, fortalecendo propósitos.
+              {is2FAMode ? (<><br /><span>Verifique sua identidade</span></>) : ''} 
             </Text>
+            {is2FAMode && (
+              <Text style={{color: colors.beige, marginBottom: 10, fontSize: 14}}>
+                Enviamos um código para o email informado
+              </Text>
+            )}
           </View>
 
           <View style={styles.form}>
-            <Field
+
+            {apiError ? <Text style={styles.errorText}>{apiError}</Text> : null}
+
+            {!is2FAMode ? (
+              // Vinicius: Modo login normal
+              <>
+                <Field
+                  label="Email"
+                  value={email}
+                  onChange={setEmail}
+                  error={errors.email}
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                />
+                <Field 
+                  label="Senha"
+                  value={password}
+                  onChange={setPassword}
+                  secure
+                  error={errors.password}
+                />
+                <Pressable style={styles.button} onPress={HandleSignin} disabled={isLoading}>
+                  {isLoading ? (
+                    <ActivityIndicator color={colors.beige} />
+                  ) : (
+                    <Text style={styles.buttonText}>Acessar</Text>
+                  )}
+                </Pressable>
+                {/* <Link href='/(auth)/signin/LoginPage)' style={styles.link}> */}
+                  <Text>Ainda não possui uma conta? Cadastre-se</Text>
+                {/* </Link> */}
+              </>
+            ) : (
+              <>
+                <Field 
+                  label="Código de verificação"
+                  value={twoFactorCode}
+                  onChange={setTwoFactorCode}
+                  error={errors.code}
+                  keyboardType="numeric"
+                  maxLength={6}
+                  placeholder="000000"
+                />
+                <Pressable style={styles.button} onPress={HandleConfirm2FA} disabled={isLoading}>
+                  {isLoading ? (
+                    <ActivityIndicator color={colors.beige}/>
+                  ) : (
+                    <Text style={styles.buttonText}>Validar Código</Text>
+                  )}
+                </Pressable>
+
+                <TouchableOpacity style={styles.link} onPress={() => {
+                  setIs2FAMode(false)
+                  setApiError('')
+                }}>
+                  <Text style={{color: colors.orangeDark}}>Voltar para login</Text>
+                  
+                </TouchableOpacity>
+              </>
+            )}
+            {/* <Field
               label="Email"
               value={email}
               onChange={setEmail}
@@ -73,37 +172,38 @@ export default function Login() {
               onChange={setPassword}
               secure
               error={errors.password}
-            />
+            /> */}
 
-            <Pressable style={styles.button} onPress={HandleSignin}>
-              <Text style={styles.buttonText}>{loading ? 'Carregando...' : 'Acessar'}</Text>
-            </Pressable>
+            {/* // <Pressable style={styles.button} onPress={HandleSignin}>
+            //   <Text style={styles.buttonText}>{loading ? 'Carregando...' : 'Acessar'}</Text>
+            // </Pressable>
 
-            <Link href='/(auth)/signup/page' style={styles.link}>
-              <Text>Ainda não possui uma conta? Cadastre-se</Text>
-            </Link>
+            // <Link href='/(auth)/signup/page' style={styles.link}>
+            //   <Text>Ainda não possui uma conta? Cadastre-se</Text>
+            // </Link> */}
           </View>
         </LinearGradient>
       </ScrollView>
-    </KeyboardAvoidingView>
+    </SafeAreaView>
 
 
 
   );
 }
 
-function Field({ label, value, onChange, secure, error }: any) {
+function Field({ label, value, onChange, secure, error, ...props }: any) {
   return (
     <View>
       <Text style={[styles.label, error && styles.labelError]}>
         {label} {error && '*'}
       </Text>
       <TextInput
-        placeholder={label + '...'}
+        placeholder={props.placeholder || label + '...'}
         style={[styles.input, error && styles.inputError]}
         value={value}
         onChangeText={onChange}
         secureTextEntry={secure}
+        {... props}
       />
     </View>
   )
@@ -112,7 +212,8 @@ function Field({ label, value, onChange, secure, error }: any) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: paddingTop
+    paddingTop: paddingTop,
+    minHeight: '100%'
   },
   header: {
     paddingLeft: 14,
@@ -140,6 +241,7 @@ const styles = StyleSheet.create({
     paddingTop: 24,
     paddingLeft: 14,
     paddingRight: 14,
+    paddingBottom: 40
   },
   label: {
     fontFamily: 'Nunito',
@@ -183,5 +285,15 @@ const styles = StyleSheet.create({
     color: colors.red,
     marginBottom: 10,
     fontSize: 16
+  },
+  errorDisplay: {
+    color: colors.red,
+    marginBottom: 16,
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#ffe6e6', // Fundo leve para destacar o erro
+    padding: 10,
+    borderRadius: 8,
   }
 });
