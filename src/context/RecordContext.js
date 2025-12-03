3e5ad1efd375f53749850e2b0cb55a9c7e34b9e5
ProjetoSofis <projetosofis@outlook.com>
2025-12-03 01:41:20 -0300

    feat: added basic methods of report

--- src/context/RecordContext.js
+++ src/context/RecordContext.js
@@ -1,27 +1,45 @@
+import axios from "axios";
 import { createContext, useContext, useState } from "react";
+import api from "../services/api";
 
 const RecordContext = createContext();
+const API_URL = api.defaults.baseURL;
+
 
 export function RecordProvider({ children }) {
-  const [record, setRecord] = useState({
-    name: "",
-    cpf: "",
-    birthDate: "",
-    responsible: "",
-    codigoEol: "",
-    endereco: "",
-    unidadeEscolar: "",
-    anoEscolar: "",
-    momName: "",
-    dadName: "",
-  });
+  const [record, setRecord] = useState(null);
+  const [reports, setReports] = useState([]);
+
+  async function loadRecord(id) {
+    const response = await axios.get(`${API_URL}/Children/${id}`);
+    setRecord(response.data);
+  }
+
+  async function createRecord(payload) {
+    const response = await axios.post(`${API_URL}/Children`, payload);
+    setRecord(response.data);
+  }
 
   function updateRecord(field, value) {
     setRecord((prev) => ({ ...prev, [field]: value }));
+    setRecord(null);
+  }
+  async function deleteRecord(id) {
+    await axios.delete(`${API_URL}/Children/${id}`);
+    setRecord(null);
+  }
+  async function loadReports(childId) {
+    const response = await axios.get(`${API_URL}/Children/${childId}/reports`);
+    setReports(response.data);
+  }
+
+  async function createReport(childId, payload) {
+    const respose = await axios.post(`${API_URL}/Children/${childId}/report`, payload);
+    return respose.data;
   }
 
   return (
-    <RecordContext.Provider value={{ record, updateRecord }}>
+    <RecordContext.Provider value={{ record, updateRecord, reports, loadRecord, loadReports, updateRecord, createRecord, createReport, deleteRecord }}>
       {children}
     </RecordContext.Provider>
   );
